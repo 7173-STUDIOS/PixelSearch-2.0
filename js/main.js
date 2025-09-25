ac9382b01c4a59b0d9e8edeea66a100ec348a0dc
PixelPaw Coding <pixelpaw-coding@keemail.me>
2025-09-25 21:54:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 async function search(query) {
     const res = await fetch("http://localhost:3000/search", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ query })
-    });
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ query: input })
+});
+
     return res.json(); // structured data
 }
 
