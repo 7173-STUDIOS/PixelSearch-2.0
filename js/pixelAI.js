ad8bb9fd4468a1e1432041ce0d1f6cf04b79a231
PixelPaw Coding <pixelpaw-coding@keemail.me>
2025-09-25 21:17:03 +0700

    Update pixelAI.js

--- js/pixelAI.js
+++ js/pixelAI.js
@@ -1,14 +1,5 @@
-
-document.getElementById("pixelAI-submit").addEventListener("click", async () => {
-  const input = document.getElementById("pixelAI-input").value;
-  const responseDiv = document.getElementById("pixelAI-response");
-
-  const res = await fetch("http://localhost:3000/pixelai", {  // your backend URL
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query: input })
-  });
-
-  const data = await res.json();
-  responseDiv.innerHTML = `<p>${data.response}</p>`;
+const res = await fetch("https://pixel-search-2-0.vercel.app/pixelai", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ query: input })
 });
